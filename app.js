c59ab08986e6a3fa8245b9dd56ef9fd596d082bb
Andrew Findlay <andrew@Pingu-Mac.local>
2018-10-05 11:42:44 +0100

    sqlite implementation

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('Scores');
+
+db.serialize(function () {
+//   db.run('CREATE TABLE lorem (info TEXT)');
+//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+
+//   for (var i = 0; i < 10; i++) {
+//     stmt.run('Ipsum ' + i);
+//   }
+
+//   stmt.finalize();
+
+  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+    console.log(row.id + ': ' + row.info);
+  });
+});
+
+db.close()
 // const angular = require('angular').Server(app);
 
 server.listen(3000);
