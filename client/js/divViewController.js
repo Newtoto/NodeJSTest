abfdea7d26411463a4afea0d43a5c9c4b4baae4f
Andrew Findlay <andrew@pingu-mac.home>
2018-11-07 16:00:12 +0000

    Removed custom hex value option

--- client/js/divViewController.js
+++ client/js/divViewController.js
@@ -3,11 +3,13 @@
 var loginDiv = document.getElementById("login");
 var gameDiv = document.getElementById("game");
 
+// Show game div, hide login div
 var enableGame = function(){
     gameDiv.setAttribute("style", "display: inline");
     loginDiv.setAttribute("style", "display: none");
 }
 
+// Show login div, hide game div
 var disableGame = function(){
     loginDiv.setAttribute("style", "display: inline");
     gameDiv.setAttribute("style", "display: none");
